f2544b887c6cec1933348b7b240ff877ccdaac8b
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-12-07 00:50:28 +0800

    clean

--- neoforge/src/main/java/org/thinkingstudio/confirmquit/client/neoforge/ConfirmQuitNeoForge.java
+++ neoforge/src/main/java/org/thinkingstudio/confirmquit/client/neoforge/ConfirmQuitNeoForge.java
@@ -2,6 +2,7 @@ package org.thinkingstudio.confirmquit.client.neoforge;
 
 import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.client.ConfigScreenHandler;
@@ -10,8 +11,9 @@ import org.thinkingstudio.confirmquit.client.config.ConfigHelper;
 
 @Mod(ConfirmQuit.MOD_ID)
 public final class ConfirmQuitNeoForge {
-    public ConfirmQuitNeoForge(ModContainer modContainer) {
+    public ConfirmQuitNeoForge() {
         if (FMLLoader.getDist().isClient()) {
+            ModContainer modContainer = ModList.get().getModContainerById(ConfirmQuit.MOD_ID).orElseThrow();
             modContainer.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (remoteVersion, isFromServer) -> true));
             modContainer.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, screen) -> ConfigHelper.getConfigScreen(screen)));
             ConfirmQuit.onInitClient();
