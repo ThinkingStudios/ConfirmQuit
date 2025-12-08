361534fd4582afbf78792d1089315820f39cf221
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-12-08 13:31:00 +0800

    clean code

--- common/src/main/java/org/thinkingstudio/confirmquit/client/toast/QuitToast.java
+++ common/src/main/java/org/thinkingstudio/confirmquit/client/toast/QuitToast.java
@@ -1,6 +1,5 @@
 package org.thinkingstudio.confirmquit.client.toast;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.toast.ToastManager;
 import net.minecraft.text.Text;
@@ -24,8 +23,6 @@ public class QuitToast extends BaseToast {
 
     @Override
     protected void drawToast(DrawContext drawContext, ToastManager manager) {
-        RenderSystem.setShaderTexture(0, texture);
-        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
         drawContext.drawTexture(texture, 0, 0, 0, 0, getWidth(), getHeight());
         drawContext.drawTexture(texture, 8, 0, 242, 0, 15, 30);
         drawContext.drawTextWithShadow(manager.getClient().textRenderer, title, 35, 7, Color.WHITE.getRGB());
