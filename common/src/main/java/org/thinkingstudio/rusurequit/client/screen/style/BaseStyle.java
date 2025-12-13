e580ff8f8154f843fb6e7f3096a318a44145d561
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-12-13 16:05:27 +0800

    clean

--- common/src/main/java/org/thinkingstudio/rusurequit/client/screen/style/BaseStyle.java
+++ common/src/main/java/org/thinkingstudio/rusurequit/client/screen/style/BaseStyle.java
@@ -6,13 +6,9 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.render.*;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 
 public abstract class BaseStyle {
-    public static final Identifier INWORLD_MENU_BACKGROUND_TEXTURE = new Identifier("textures/gui/inworld_menu_background.png");
-
     public abstract ButtonWidget generateConfirmButtons(Screen screen, ButtonWidget.PressAction onConfirm);
 
     public abstract ButtonWidget generateCancelButtons(Screen screen, ButtonWidget.PressAction onCancel);
@@ -23,11 +19,4 @@ public abstract class BaseStyle {
 
     public abstract void render(MinecraftClient client, TextRenderer textRenderer, Screen screen, Text title, Text message,
                                 DrawContext drawContext, int mouseX, int mouseY, float delta);
-
-    protected void renderBackground(int startX, int startY, int endX, int endY, Identifier identifier, DrawContext drawContext) {
-        int width = endX - startX;
-        int height = endY - startY;
-        drawContext.drawTexture(identifier, startX + 4, startY + 4, 0, 0, width - 4, height - 4, 32, 32);
-        drawContext.fillGradient(startX, startY, endX, endY, -1072689136, -804253680);
-    }
 }
